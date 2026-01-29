cf7fa0b254803de6172b900020f177e466b0ca5e
weisong.zhao <weisong.zhao@wbstar.com>
2026-01-29 21:37:55 +0800

    feat: add 域名配置

--- avalon-web/frontend/vite.config.ts
+++ avalon-web/frontend/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: ['ai.easycloud.vip'],
     proxy: {
       '/api': {
         target: 'http://0.0.0.0:8000',
